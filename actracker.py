e5cc6b2b6364301cbc5014295394f3ab285cdfe1
Mathias Andre <mathias@acronycal.org>
2015-10-17 23:04:05 +0200

    Remove useless os.path.join

--- actracker.py
+++ actracker.py
@@ -21,8 +21,7 @@ import sys
 import traceback
 
 sys.path.insert(
-    0, os.path.join(os.path.dirname(__file__),
-                    'actrackerDLL/%s' % platform.architecture()[0])
+   0, 'apps/python/actracker/actrackerDLL/%s/' % platform.architecture()[0]
 )
 from actrackerDLL.sim_info import info
 
